4b997df409e979b47abc574aee00a9e0912f3bea
aayusha19 <aayushashrestha19@gmail.com>
2025-02-24 08:50:56 +0545

    Made some changes

--- priority.cpp
+++ priority.cpp
@@ -1,11 +1,11 @@
 #include <queue>
 #include "car.cpp"
 
-// Custom comparator for priority queue
+
 struct CompareSpeed {
     bool operator()(SmallCar const& c1, SmallCar const& c2) {
         return c1.getSpeed() < c2.getSpeed(); // Higher speed gets priority
     }
 };
 
-// Usage: std::priority_queue<SmallCar, std::vector<SmallCar>, CompareSpeed> carQueue;
+
--- src/main.cpp
+++ src/main.cpp
@@ -95,5 +95,5 @@ int main(int argc, char* argv[]) {
     simulator.run();
     simulator.cleanup();
 
-    return 0;
+    return 1;
 }
